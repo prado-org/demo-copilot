3f9b8369a817c996fe98b5cb4f3c5f18e93a5c28
Leandro Prado <leadro@microsoft.com>
2024-02-06 12:05:01 -0300

    Update project references and namespaces

---
+++ Todo.UnitTest/Model/Todo.cs
@@ -0,0 +1,9 @@
+namespace Todo.Test.Model
+{
+    public class TodoModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool Completed { get; set; }
+    }
+}
--- Todo.UnitTest/UnitTest1.cs
+++ Todo.UnitTest/UnitTest1.cs
@@ -1,7 +1,7 @@
 using System.Net;
 using System.Net.Http.Json;
 using Microsoft.AspNetCore.Mvc.Testing;
-using Todo.Api.Model;
+using Todo.Test.Model;
 
 namespace Todo.UnitTest;
 
